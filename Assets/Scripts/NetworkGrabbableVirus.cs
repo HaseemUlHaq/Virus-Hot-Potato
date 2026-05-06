2154cc006466077bfec118d0ec6974125d4e6081
Lorena Livadaru <livadaru.design@gmail.com>
2026-05-06 17:17:58 +0200

    feat: explosion on grab add debugManager

--- Assets/Scripts/NetworkGrabbableVirus.cs
+++ Assets/Scripts/NetworkGrabbableVirus.cs
@@ -1,38 +1,200 @@
 using UnityEngine;
 using Fusion;
 using Oculus.Interaction;
+using TMPro;
 
 [RequireComponent(typeof(NetworkObject))]
 [RequireComponent(typeof(Grabbable))]
 public class NetworkGrabbableVirus : NetworkBehaviour
 {
+    [SerializeField] private float fuseDurationSeconds = 10f;
+    [SerializeField] private GameObject explosionPrefab;
+    [SerializeField] private ParticleSystem explosionFxFallback;
+    [SerializeField] private TextMeshProUGUI eliminationMessageText;
+
+    [Networked] private TickTimer FuseTimer { get; set; }
+    [Networked] private NetworkBool FuseStarted { get; set; }
+    [Networked] public NetworkBool RoundResolved { get; private set; }
+    [Networked] private NetworkBool HasElimination { get; set; }
+    [Networked] public PlayerRef EliminatedPlayer { get; private set; }
+    [Networked] private NetworkBool HasHolder { get; set; }
+    [Networked] private PlayerRef CurrentHolder { get; set; }
+    [Networked] private NetworkBool HasLastTouchedPlayer { get; set; }
+    [Networked] private PlayerRef LastTouchedPlayer { get; set; }
+
+    public bool DebugFuseStarted => FuseStarted;
+    public bool DebugHasHolder => HasHolder;
+    public bool DebugHasElimination => HasElimination;
+    public PlayerRef DebugCurrentHolder => CurrentHolder;
+    public PlayerRef DebugLastTouchedPlayer => LastTouchedPlayer;
+
     private Grabbable _grabbable;
     private Rigidbody _rb;
+    private ChangeDetector _changeDetector;
+    private bool _localEliminationAnnounced;
+    private bool _grabDisabledLocally;
+    private bool _localExplosionPlayed;
 
     public override void Spawned()
     {
         _grabbable = GetComponent<Grabbable>();
         _rb = GetComponent<Rigidbody>();
         _grabbable.WhenPointerEventRaised += OnPointerEvent;
+        _changeDetector = GetChangeDetector(ChangeDetector.Source.SnapshotTo, false);
+
+        if (HasStateAuthority)
+        {
+            FuseTimer = TickTimer.CreateFromSeconds(Runner, fuseDurationSeconds);
+            FuseStarted = true;
+            RoundResolved = false;
+            HasHolder = false;
+            HasElimination = false;
+            HasLastTouchedPlayer = false;
+        }
+
+        if (RoundResolved)
+            ApplyRoundEndedLocal();
+    }
+
+    public override void FixedUpdateNetwork()
+    {
+        if (HasStateAuthority && !FuseStarted)
+        {
+            FuseTimer = TickTimer.CreateFromSeconds(Runner, fuseDurationSeconds);
+            FuseStarted = true;
+            RoundResolved = false;
+            HasHolder = false;
+            HasElimination = false;
+            HasLastTouchedPlayer = false;
+        }
+
+        if (RoundResolved)
+        {
+            ApplyRoundEndedLocal();
+            return;
+        }
+
+        if (HasStateAuthority && FuseStarted && FuseTimer.Expired(Runner))
+            ResolveRound();
+    }
+
+    // Phase 1: last touched player loses if virus is in flight at fuse end.
+    private void ResolveRound()
+    {
+        RoundResolved = true;
+        if (HasHolder)
+        {
+            HasElimination = true;
+            EliminatedPlayer = CurrentHolder;
+        }
+        else if (HasLastTouchedPlayer)
+        {
+            HasElimination = true;
+            EliminatedPlayer = LastTouchedPlayer;
+        }
+        else
+        {
+            HasElimination = false;
+        }
+
+        Debug.Log($"[HotPotato] Fuse expired. Holder={CurrentHolder}, LastTouched={LastTouchedPlayer}, HasHolder={HasHolder}");
+
+        if (_rb != null)
+        {
+            if (!_rb.isKinematic)
+            {
+                _rb.linearVelocity = Vector3.zero;
+                _rb.angularVelocity = Vector3.zero;
+            }
+            _rb.isKinematic = true;
+        }
+    }
+
+    public override void Render()
+    {
+        foreach (var change in _changeDetector.DetectChanges(this))
+        {
+            if (change != nameof(RoundResolved) || !RoundResolved)
+                continue;
+        }
+
+        // Do not rely on a single change-detection frame for one-shot visuals.
+        if (RoundResolved && !_localExplosionPlayed)
+        {
+            _localExplosionPlayed = true;
+            if (explosionPrefab != null)
+                Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            else if (explosionFxFallback != null)
+                explosionFxFallback.Play();
+        }
+
+        if (!RoundResolved || !HasElimination || _localEliminationAnnounced)
+            return;
+
+        if (EliminatedPlayer != Runner.LocalPlayer)
+            return;
+
+        _localEliminationAnnounced = true;
+        Debug.Log("[HotPotato] You are eliminated.");
+        if (eliminationMessageText != null)
+        {
+            eliminationMessageText.gameObject.SetActive(true);
+            eliminationMessageText.text = "You are out!";
+        }
+    }
+
+    public float GetRemainingSeconds()
+    {
+        if (!FuseStarted || RoundResolved)
+            return 0f;
+
+        float remaining = FuseTimer.RemainingTime(Runner).GetValueOrDefault(0f);
+        return Mathf.Max(remaining, 0f);
+    }
+
+    private void ApplyRoundEndedLocal()
+    {
+        if (_grabDisabledLocally)
+            return;
+        _grabDisabledLocally = true;
+        if (_grabbable != null)
+            _grabbable.enabled = false;
     }
 
     private void OnPointerEvent(PointerEvent evt)
     {
+        if (RoundResolved)
+            return;
+
         switch (evt.Type)
         {
             case PointerEventType.Select:
-                // Request authority so THIS client can move it
-                Object.RequestStateAuthority();
-                // Freeze rigidbody while grabbing
-                if (_rb != null) _rb.isKinematic = true;
+                RPC_NotifyGrab(Runner.LocalPlayer);
                 break;
 
             case PointerEventType.Unselect:
-                // Release authority so others can grab it
-                if (_rb != null) _rb.isKinematic = false;
-                Object.ReleaseStateAuthority();
+                RPC_NotifyRelease();
                 break;
         }
+    }
+
+    [Rpc(RpcSources.All, RpcTargets.StateAuthority)]
+    private void RPC_NotifyGrab(PlayerRef player)
+    {
+        if (RoundResolved)
+            return;
+        HasHolder = true;
+        CurrentHolder = player;
+        HasLastTouchedPlayer = true;
+        LastTouchedPlayer = player;
+    }
+
+    [Rpc(RpcSources.All, RpcTargets.StateAuthority)]
+    private void RPC_NotifyRelease()
+    {
+        if (RoundResolved)
+            return;
+        HasHolder = false;
     }
 
     public override void Despawned(NetworkRunner runner, bool hasState)
--- Assets/Scripts/VirusSpawner.cs
+++ Assets/Scripts/VirusSpawner.cs
@@ -1,20 +1,90 @@
 using Fusion;
+using Fusion.Sockets;
+using System;
+using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
 
-public class VirusSpawner : MonoBehaviour
+public class VirusSpawner : MonoBehaviour, INetworkRunnerCallbacks
 {
     public NetworkObject VirusPrefab;
-    private bool _virusSpawned = false;
+    [SerializeField] private Vector3 spawnPosition = new Vector3(0f, 0.05f, 0f);
 
-    public void PlayerJoined(NetworkRunner runner, PlayerRef player)
+    private bool _virusSpawned;
+    private readonly List<NetworkRunner> _registeredRunners = new List<NetworkRunner>();
+    private Coroutine _runnerDiscoveryRoutine;
+
+    private void OnEnable()
     {
-        if (_virusSpawned) return;
+        RegisterToActiveRunners();
+        _runnerDiscoveryRoutine = StartCoroutine(DiscoverRunnersRoutine());
+    }
 
-        // Only the master client spawns the virus
-        if (runner.IsSharedModeMasterClient)
+    private void OnDisable()
+    {
+        if (_runnerDiscoveryRoutine != null)
+            StopCoroutine(_runnerDiscoveryRoutine);
+        UnregisterFromRunners();
+    }
+
+    private IEnumerator DiscoverRunnersRoutine()
+    {
+        while (true)
         {
-            runner.Spawn(VirusPrefab, new Vector3(0, 0.05f, 0), Quaternion.identity);
-            _virusSpawned = true;
+            RegisterToActiveRunners();
+            yield return new WaitForSeconds(1f);
         }
     }
+
+    private void RegisterToActiveRunners()
+    {
+        NetworkRunner[] runners = FindObjectsByType<NetworkRunner>(FindObjectsSortMode.None);
+        foreach (NetworkRunner runner in runners)
+        {
+            if (runner == null || _registeredRunners.Contains(runner))
+                continue;
+
+            runner.AddCallbacks(this);
+            _registeredRunners.Add(runner);
+        }
+    }
+
+    private void UnregisterFromRunners()
+    {
+        foreach (NetworkRunner runner in _registeredRunners)
+        {
+            if (runner != null)
+                runner.RemoveCallbacks(this);
+        }
+
+        _registeredRunners.Clear();
+    }
+
+    public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
+    {
+        if (_virusSpawned || VirusPrefab == null || runner == null || !runner.IsSharedModeMasterClient)
+            return;
+
+        runner.Spawn(VirusPrefab, spawnPosition, Quaternion.identity);
+        _virusSpawned = true;
+    }
+
+    public void OnPlayerLeft(NetworkRunner runner, PlayerRef player) { }
+    public void OnInput(NetworkRunner runner, NetworkInput input) { }
+    public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input) { }
+    public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason) { }
+    public void OnConnectedToServer(NetworkRunner runner) { }
+    public void OnDisconnectedFromServer(NetworkRunner runner, NetDisconnectReason reason) { }
+    public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token) { }
+    public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason) { }
+    public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message) { }
+    public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList) { }
+    public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data) { }
+    public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken) { }
+    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ReliableKey key, ArraySegment<byte> data) { }
+    public void OnReliableDataProgress(NetworkRunner runner, PlayerRef player, ReliableKey key, float progress) { }
+    public void OnSceneLoadDone(NetworkRunner runner) { }
+    public void OnSceneLoadStart(NetworkRunner runner) { }
+    public void OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
+    public void OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
 }
